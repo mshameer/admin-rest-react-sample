dcaa823719a15353e0620747b868892bb82af08a
qshameer <mohameds@qburst.com>
2017-10-26 17:38:43 +0530

    added auth

--- src/campaigns/details.js
+++ src/campaigns/details.js
@@ -1,86 +1,37 @@
-import React from 'react';
-import { Edit, Create, Datagrid, TextField, Responsive, TabbedForm, ReferenceField,
-  EditButton, SelectInput, TextInput, FormTab, CheckboxGroupInput, ReferenceInput, AutocompleteInput, SelectField  } from 'admin-on-rest';
-import SimpleList from '../mui/list/simpleList'
-import List from '../mui/list';
-import Avatar from 'material-ui/Avatar';
-import Actors from 'material-ui/svg-icons/av/recent-actors';
-import { getCurrentUser} from '../utils/permissions';
+import React, { createElement } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Restricted from 'admin-on-rest/lib/auth/Restricted';
+import { GuestList} from './guests';
 
-const campaignStatus = [
-  { id: 'notStarted', name: 'Not Started' },
-  { id: 'inProgress', name: 'In Progress' },
-  { id: 'completed', name: 'Completed' },
-];
+const restrictPage = (component, route) => {
+  const commonProps = {
+      resource: 'guests',
+      hasList: false,
+      hasEdit: false,
+      hasShow: false,
+      hasCreate: false,
+      hasDelete: false,
+  };
 
-const actionChoices = [
-  { id: '1', name: 'Peace Radio Installation' },
-  { id: '2', name: 'Nerpadham Indroduced' },
-  { id: '3', name: 'Jeevitham Enthinu Vendi' },
-  { id: '4', name: 'Kathorkkuka Srushttavinuvendi' },
-  { id: '5', name: 'Quran Class Invited' },
-]
+    const RestrictedPage = routeProps => (
+        <Restricted authParams={{ resource: 'guests', route }} {...routeProps}>
+            {createElement(component, {
+                ...commonProps,
+                ...routeProps,
+            })}
+        </Restricted>
+    );
+    return RestrictedPage;
+};
 
-const placeChoices = [
-  { id: 1, name: 'Karikode' },
-  { id: 2, name: 'Chanthanathuthope' },
-  { id: 3, name: 'Mamoodu' },
-  { id: 4, name: 'Kadappakkada' },
-  { id: 5, name: 'Kadamukku' },
-];
+const Details = () => (
+  <Switch>
+    <Route
+        exact
+        path={`/campaigns-details`}
+        render={restrictPage(GuestList, 'list')}
+    />
+  </Switch>
+)
 
-const occupationChoices = [
-  { id: 0, name: 'Driver' },
-  { id: 1, name: 'Teacher' },
-  { id: 2, name: 'Professor' },
-  { id: 3, name: 'Gov. Employee' },
-  { id: 4, name: 'Doctor' },
-  { id: 5, name: 'Engineer' },
-];
-
-const responseChoices = [
-  { id: 0, name: 'More materials Needed' },
-  { id: 1, name: 'Nerpadham Subscription' },
-  { id: 2, name: 'Interested in Dawa Activities' },
-  { id: 3, name: 'Counselling Needed' },
-  { id: 4, name: 'Quran copy Needed' },
-  { id: 5, name: 'Help/Aid Needed' },
-];
-
-const nextActionChoices = [
-  { id: 0, name: 'Visit Again' },
-];
-
-const currentUser = getCurrentUser();
-
-const GuestList = (props) => (
-  <List {...props} title="Guests">
-    <Responsive
-      small={
-        <SimpleList
-          leftAvatar={record => <Avatar icon={<Actors />} />}
-        >
-          <TextField source="name" type="primary" />
-          <TextField source="phoneNo" type="secondary" />
-          <SelectField source="place" choices={placeChoices} type="secondaryPlus"  />
-          <SelectField source="status" choices={campaignStatus} type="tertiary"  />
-        </SimpleList>
-      }
-      medium={
-        <Datagrid>
-          <TextField label="Name" source="name" />
-          <TextField label="Phone No" source="phoneNo" />
-          <SelectField source="place" choices={placeChoices} label="Place"  />
-          <SelectField label="Next Action"  source="nextAction" choices={nextActionChoices}  />
-          <ReferenceField label="Referred by" source="referredBy" reference="users" >
-            <TextField source="displayName" />
-          </ReferenceField>
-          <SelectField source="status" choices={campaignStatus}  />
-          <EditButton />
-        </Datagrid>
-      }
-      />
-  </List>
-);
-
-export const Details = (props) => <GuestList resource="guests" {...props} />
+export default Details;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { Details } from './campaigns/details';
+import Details from './campaigns/details';
 
 export default [
     <Route exact path="/campaigns-details" component={Details} />
